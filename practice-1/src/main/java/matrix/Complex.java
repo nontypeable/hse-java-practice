761f49bf9ecba898deccea9fb1f9c71327a542f7
Andrey Sadkov <ad.sadkov@gmail.com>
2026-09-21 15:53:25 +0300

    feat(complex): add `Complex` record

---
+++ practice-1/src/main/java/matrix/Complex.java
@@ -0,0 +1,34 @@
+package matrix;
+
+public record Complex(double re, double im) {
+    public Complex add(Complex other) {
+        return new Complex(
+            re + other.re,
+            im + other.im
+        );
+    }
+
+    public Complex substract(Complex other) {
+        return new Complex(
+            re - other.re,
+            im - other.im
+        );
+    }
+
+    public Complex multiply(Complex other) {
+        return new Complex(
+            re * other.re - im * other.im,
+            re * other.im + im * other.re
+        );
+    }
+
+    public Complex devide(Complex other) {
+        double denominator = other.re * other.re + other.im * other.im;
+        if (denominator == 0) {
+            throw new ArithmeticException("Division by zero");
+        }
+        return new Complex(
+            (re * other.re + im * other.im) / denominator,
+            (im * other.re - re * other.im) / denominator);
+    }
+}
